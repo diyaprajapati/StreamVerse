1d548f25ae185526f641da5fadc2041e4ac6d95d
diyaprajapati <diyabprajapati2005@gmail.com>
2025-05-08 18:03:07 +0530

    initialize frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[var(--background)] text-[var(--foreground)]">
+      {/* All other routes/components go here */}
+      helo
+    </div>
+  )
+}
